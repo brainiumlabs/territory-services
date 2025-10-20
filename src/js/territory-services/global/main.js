32585a9aaddc705c2b01ba357b19ca90734b2cdb
Alex Phan <alex@brainiumlabs.com.au>
2025-10-21 05:20:33 +0930

    Auth0 basic setup

--- src/js/territory-services/global/main.js
+++ src/js/territory-services/global/main.js
@@ -7,14 +7,14 @@ $(document).ready(function () {
 
   function addValidInvalidClasses(fieldInput) {
     var value = fieldInput.val().trim();
-    
+
     if (value === "") {
       fieldInput.removeClass("is-valid");
       fieldInput.prop("required") && fieldInput.addClass("is-invalid");
     } else {
       // Remove any existing validation classes
       fieldInput.removeClass("is-valid is-invalid");
-      
+
       // Check the validity
       if (fieldInput[0].checkValidity()) {
         fieldInput.addClass("is-valid");
@@ -24,7 +24,7 @@ $(document).ready(function () {
     }
   }
 
-  $("input, textarea, select").blur(function() {
+  $("input, textarea, select").blur(function () {
     addValidInvalidClasses($(this));
   });
 
@@ -76,7 +76,58 @@ $(document).ready(function () {
       $(this).removeClass("external");
     });
   }
-  
+
+  // Add Auth0 login/logout functionality
+  auth0
+    .createAuth0Client({
+      domain: "dev.ntg-dcdd.auth0app.com",
+      clientId: "rWBUjZnqbBIIrjen52u3Q3Lz6XGVsACL",
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    })
+    .then(async (auth0Client) => {
+      // Assumes a button with id "login" in the DOM
+      const loginButton = document.getElementById("login");
+
+      loginButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        auth0Client.loginWithRedirect();
+      });
+
+      if (
+        location.search.includes("state=") &&
+        (location.search.includes("code=") ||
+          location.search.includes("error="))
+      ) {
+        await auth0Client.handleRedirectCallback();
+        window.history.replaceState({}, document.title, "/");
+      }
+
+      // Assumes a button with id "logout" in the DOM
+      const logoutButton = document.getElementById("logout");
+
+      logoutButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        auth0Client.logout();
+      });
+
+      const isAuthenticated = await auth0Client.isAuthenticated();
+      const userProfile = await auth0Client.getUser();
+
+      // Assumes an element with id "profile" in the DOM
+      const profileElement = document.getElementById("profile");
+
+      if (isAuthenticated) {
+        profileElement.style.display = "block";
+        profileElement.innerHTML = `
+          <p>${userProfile.name}</p>
+          <img src="${userProfile.picture}" />
+        `;
+      } else {
+        profileElement.style.display = "none";
+      }
+    });
 
   handleAlertBanner(); //call handleAlretBanner function
   externalLinksInNewWindow(); //call externalLinksInNewWindow function
