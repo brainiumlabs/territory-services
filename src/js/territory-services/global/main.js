3f8c9977d43f6f9c7d3fd9883c8abdd23a290e42
Alex Phan <alex@brainiumlabs.com.au>
2025-10-27 08:03:17 +0930

    remove auth0 config; put it in matrix directly for control

--- src/js/territory-services/global/main.js
+++ src/js/territory-services/global/main.js
@@ -77,58 +77,6 @@ $(document).ready(function () {
     });
   }
 
-  // Add Auth0 login/logout functionality
-  auth0
-    .createAuth0Client({
-      domain: "dev-ksjpo8ohb35kp4b7.au.auth0.com",
-      clientId: "x8W7MIENJPaWbBniAkEsEOd0QWVjWJKS",
-      authorizationParams: {
-        redirect_uri: window.location.origin,
-      },
-    })
-    .then(async (auth0Client) => {
-      // Assumes a button with id "login" in the DOM
-      const loginButton = document.getElementById("login");
-
-      loginButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        auth0Client.loginWithRedirect();
-      });
-
-      if (
-        location.search.includes("state=") &&
-        (location.search.includes("code=") ||
-          location.search.includes("error="))
-      ) {
-        await auth0Client.handleRedirectCallback();
-        window.history.replaceState({}, document.title, "/");
-      }
-
-      // Assumes a button with id "logout" in the DOM
-      const logoutButton = document.getElementById("logout");
-
-      logoutButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        auth0Client.logout();
-      });
-
-      const isAuthenticated = await auth0Client.isAuthenticated();
-      const userProfile = await auth0Client.getUser();
-
-      // Assumes an element with id "profile" in the DOM
-      const profileElement = document.getElementById("profile");
-
-      if (isAuthenticated) {
-        profileElement.style.display = "block";
-        profileElement.innerHTML = `
-          <p>${userProfile.name}</p>
-          <img src="${userProfile.picture}" />
-        `;
-      } else {
-        profileElement.style.display = "none";
-      }
-    });
-
   handleAlertBanner(); //call handleAlretBanner function
   externalLinksInNewWindow(); //call externalLinksInNewWindow function
 });
